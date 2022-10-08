c3efb7997a49b710d7ed399aa9e8bc13a02bc9d4
pino._.hoo <inhoo23@naver.com>
2022-10-08 12:38:45 +0900

    add : Login Screen

--- pages/LoginScreen.tsx
+++ pages/LoginScreen.tsx
@@ -1,23 +1,80 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {create} from 'react-test-renderer';
+import React from 'react'
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 
-export default function HomeScreen({navigation}: any) {
-  const onPress = () => {
-    console.log(1);
-    navigation.navigate('Root');
-  };
+export default function LoginScreen({navigation}: any) {
+  const onPressJoinBtn = () => {
+    navigation.navigate('Register')
+  }
+
+  const opPressLogin = () => {
+    navigation.navigate('Root')
+  }
+
   return (
     <View style={styles.container}>
-      <Text onPress={onPress}>LoginScreeen</Text>
+      <View style={styles.logoBox}>
+        <Image style={styles.img} source={require('../assets/logo.png')} />
+      </View>
+      <View style={styles.bodyBox}>
+        <TextInput placeholder="id" style={styles.textInput} />
+        <TextInput placeholder="password" style={styles.textInput} />
+        <TouchableOpacity style={styles.btn} onPress={opPressLogin}>
+          <Text style={styles.btnText}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={onPressJoinBtn}>
+          <Text style={styles.btnText}>JOIN</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
   },
-});
+  logoBox: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bodyBox: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: -100,
+  },
+  img: {
+    width: 200,
+    height: 145,
+  },
+  textInput: {
+    width: 240,
+    height: 45,
+    backgroundColor: '#D9D9D9',
+    marginBottom: 20,
+    borderRadius: 10,
+    padding: 10,
+  },
+  btn: {
+    width: 240,
+    height: 45,
+    backgroundColor: '#C47DFF',
+    marginBottom: 20,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 20,
+  },
+})
