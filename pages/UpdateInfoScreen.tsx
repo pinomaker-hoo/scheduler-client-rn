3ed26b6138deb79826efa0b8c3bcf7288d01d40e
pino._.hoo <inhoo23@naver.com>
2022-11-07 06:09:42 +0900

    Update UpdateInfoScreen.tsx

--- pages/UpdateInfoScreen.tsx
+++ pages/UpdateInfoScreen.tsx
@@ -1,42 +1,112 @@
-import React, {useState} from 'react'
-import {StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import {
+  Alert,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import {imgToBase64Code, nullCheck} from '../common/common'
+import {launchImageLibrary} from 'react-native-image-picker'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import {deleteUser, updateImg, updatePassword} from '../api/auth'
 
 export default function UpdateInfoScreen({navigation}: any) {
-  const [name, setName] = useState('')
-  const [id, setId] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordc, setPasswordc] = useState('')
+  const [photo, setPhoto]: any = useState(null)
+  const [user, setUser] = useState()
 
-  const onPress = () => {
-    navigation.navigate('Root')
+  useEffect(() => {
+    callApi()
+  }, [])
+
+  const callApi = async () => {
+    const user = await AsyncStorage.getItem('user')
+    const jsonParser = user && (await JSON.parse(user))
+    setUser(jsonParser)
   }
+
+  const handleChoosePhoto = () => {
+    launchImageLibrary({mediaType: 'photo'}, (response: any) => {
+      if (response) {
+        setPhoto(response.assets[0])
+      }
+    })
+  }
+
+  const onPressUpdate = async () => {
+    const base = await imgToBase64Code(photo.uri)
+    const {data}: any = await updateImg(base)
+    if (!data) return Alert.alert('ERROR')
+    navigation.navigate('메인')
+  }
+
+  const onPressChangePassword = () => {
+    Alert.prompt('변경할 비밀번호를 입력하세요.', '', [
+      {
+        text: '아니요.',
+        style: 'cancel',
+      },
+      {
+        text: '네',
+        onPress: (value: any) => updatePasswordFunc(value),
+      },
+    ])
+  }
+
+  const onPressDeleteUser = async () => {
+    Alert.alert('정말로 탈퇴하시나요.', '', [
+      {
+        text: '아니요.',
+        style: 'cancel',
+      },
+      {
+        text: '네',
+        onPress: deleteFunc,
+      },
+    ])
+  }
+
+  const deleteFunc = async () => {
+    const {data}: any = await deleteUser()
+    if (!data) return Alert.alert('ERROR')
+    return LogoutFunc()
+  }
+
+  const updatePasswordFunc = (value: string) => {
+    console.log(value)
+    const {data}: any = updatePassword(value)
+    if (!data) return Alert.alert('ERROR')
+    return navigation.navigate('개인')
+  }
+
+  const LogoutFunc = async () => {
+    await AsyncStorage.removeItem('accesstoken')
+    navigation.navigate('Login')
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>내 프로필</Text>
-      <TextInput
-        onChangeText={name => setName(name)}
-        placeholder="이름"
-        style={styles.textInput}
-      />
-      <TextInput
-        onChangeText={id => setId(id)}
-        placeholder="생년월일"
-        style={styles.textInput}
-      />
-      <TextInput
-        onChangeText={password => setPassword(password)}
-        placeholder="비밀번호 변경"
-        secureTextEntry={true}
-        style={styles.textInput}
-      />
-      <TextInput
-        onChangeText={passwordc => setPasswordc(passwordc)}
-        placeholder="비밀번호 확인"
-        secureTextEntry={true}
-        style={styles.textInput}
-      />
-      <TouchableOpacity style={styles.btn} onPress={onPress}>
-        <Text style={styles.btnText}>JOIN</Text>
+      {photo ? (
+        <TouchableOpacity onPress={handleChoosePhoto}>
+          <Image style={styles.img} source={{uri: photo.uri}} />
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity onPress={handleChoosePhoto}>
+          <Image style={styles.img} source={require('../assets/user.png')} />
+        </TouchableOpacity>
+      )}
+      <Text style={styles.bodyText}>김도연</Text>
+      <TouchableOpacity style={styles.btn} onPress={onPressChangePassword}>
+        <Text style={styles.btnText}>비밀번호 변경</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btn} onPress={onPressDeleteUser}>
+        <Text style={styles.btnText}>회원 탈퇴</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btn} onPress={onPressUpdate}>
+        <Text style={styles.btnText}>수정 완료</Text>
       </TouchableOpacity>
     </View>
   )
@@ -53,7 +123,7 @@ const styles = StyleSheet.create({
     width: 240,
     height: 45,
     backgroundColor: '#C47DFF',
-    marginBottom: 20,
+    marginTop: 20,
     borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
@@ -66,7 +136,7 @@ const styles = StyleSheet.create({
     width: 240,
     height: 45,
     backgroundColor: '#D9D9D9',
-    marginBottom: 20,
+    marginTop: 20,
     borderRadius: 10,
     padding: 10,
   },
@@ -74,5 +144,17 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: '#C47DFF',
     marginBottom: 30,
+  },
+  img: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: '#E8E8E8',
+    marginTop: 30,
+  },
+  bodyText: {
+    marginTop: 30,
+    marginBottom: 30,
+    fontSize: 30,
   },
 })
