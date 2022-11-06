b1db0329f545d3b4d013c0ac010966a33daf3a9a
pino._.hoo <inhoo23@naver.com>
2022-11-06 15:39:51 +0900

    fix : Bottom Navi

--- pages/BottomNavigator.tsx
+++ pages/BottomNavigator.tsx
@@ -4,6 +4,7 @@ import AdminScreen from './AdminScreen'
 import GroupScreen from './GroupScreen'
 import HomeScreen from './HomeScreen'
 import SearchScreen from './SearchScreen'
+import {Image, StyleSheet} from 'react-native'
 
 const BottomTab = createBottomTabNavigator()
 
@@ -17,34 +18,56 @@ export default function BottomTabNavigator() {
         name="검색"
         component={SearchScreen}
         options={{
-          title: 'Search',
-          tabBarIcon: () => null,
+          title: '',
+          tabBarIcon: () => (
+            <Image
+              style={styles.logo}
+              source={require('../assets/search.png')}
+            />
+          ),
         }}
       />
       <BottomTab.Screen
         name="그룹"
         component={GroupScreen}
         options={{
-          title: 'Group',
-          tabBarIcon: () => null,
+          title: '',
+          tabBarIcon: () => (
+            <Image
+              style={styles.logo}
+              source={require('../assets/group.png')}
+            />
+          ),
         }}
       />
       <BottomTab.Screen
         name="메인"
         component={HomeScreen}
         options={{
-          title: 'Home',
-          tabBarIcon: () => null,
+          title: '',
+          tabBarIcon: () => (
+            <Image style={styles.logo} source={require('../assets/home.png')} />
+          ),
         }}
       />
       <BottomTab.Screen
         name="개인"
         component={AdminScreen}
         options={{
-          title: 'Admin',
-          tabBarIcon: () => null,
+          title: '',
+          tabBarIcon: () => (
+            <Image
+              style={styles.logo}
+              source={require('../assets/admin.png')}
+            />
+          ),
         }}
       />
     </BottomTab.Navigator>
   )
 }
+const styles = StyleSheet.create({
+  logo: {
+    marginTop: 35,
+  },
+})
