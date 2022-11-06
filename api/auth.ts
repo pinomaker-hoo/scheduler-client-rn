920160f728d8732b988dcc6d80b2b7a16e5ebc5a
pino._.hoo <inhoo23@naver.com>
2022-11-07 05:02:57 +0900

    fix : auth Api

--- api/auth.ts
+++ api/auth.ts
@@ -8,30 +8,41 @@ const auth = axios.create({
 
 export const login = async (id: string, password: string) => {
   try {
-    return await auth({
+    const {data} = await auth({
       url: '/local',
       method: 'post',
       data: {id, password},
     })
+    return data
   } catch (err) {
     console.log(err)
   }
 }
 
 export const register = async (id: string, name: string, password: string) => {
-  return await auth({
-    url: '/',
-    method: 'post',
-    data: {id, name, password},
-  })
+  try {
+    const {data} = await auth({
+      url: '/',
+      method: 'post',
+      data: {id, name, password},
+    })
+    return data
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export const findUserById = async (id: string) => {
-  return await auth({
-    method: 'post',
-    url: '/check',
-    data: {
-      id,
-    },
-  })
+  try {
+    const {data} = await auth({
+      method: 'post',
+      url: '/check',
+      data: {
+        id,
+      },
+    })
+    return data
+  } catch (err) {
+    console.log(err)
+  }
 }
